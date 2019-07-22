4a63ad9e66ea9963622abd13e261c2df5dec783a
Brian M <bpmmor@gmail.com>
2019-07-22 14:45:32 -0400

    Add function to run a Recom chain on a given graph and population distribution

---
+++ clustering_scatter_functions.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 22 10:21:28 2019
+
+@author: Brian
+"""
+
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+
+import random
+
+import matplotlib.pyplot as plt
+from functools import partial
+import networkx as nx
+
+from gerrychain import MarkovChain
+from gerrychain.constraints import (
+    Validator,
+    single_flip_contiguous,
+    within_percent_of_ideal_population,
+)
+from gerrychain.proposals import propose_random_flip
+from gerrychain.accept import always_accept
+from gerrychain.updaters import Election, Tally, cut_edges
+from gerrychain.partition import Partition
+from gerrychain.proposals import recom
+from gerrychain.metrics import mean_median, efficiency_gap
+
+import csv
+import os
+from functools import partial
+import json
+
+import matplotlib.pyplot as plt
+
+from gerrychain import (
+    Election,
+    Graph,
+    MarkovChain,
+    Partition,
+    accept,
+    constraints,
+    updaters,
+)
+
+from gerrychain.tree import recursive_tree_part
+
+
+
+
+def run_ensemble_on_distro(graph, min_pop_col, maj_pop_col, tot_pop_col, num_districts, initial_plan, num_steps, pop_tol = 0.05, min_win_thresh = 0.5):
+    my_updaters = {
+        "population": Tally(tot_pop_col),
+        "cut_edges": cut_edges,
+        "maj-min": Election("maj-min", {"maj": maj_pop_col, "min": min_pop_col}),
+    }
+    
+    initial_partition = Partition(graph = initial_plan.graph, assignment = initial_plan.assignment, updaters = my_updaters)
+    
+    # ADD CONSTRAINTS
+    popbound = within_percent_of_ideal_population(initial_partition, 0.1)
+    
+    # ########Setup Proposal
+    ideal_population = sum(initial_partition[tot_pop_col].values()) / len(initial_partition)
+    
+    tree_proposal = partial(
+        recom,
+        pop_col=tot_pop_col,
+        pop_target=ideal_population,
+        epsilon=pop_tol,
+        node_repeats=1,
+    )
+    
+    # ######BUILD MARKOV CHAINS
+    
+    recom_chain = MarkovChain(
+        tree_proposal,
+        Validator([popbound]),
+        accept=always_accept,
+        initial_state=initial_partition,
+        total_steps=num_steps,
+    )
+    
+    cut_edges_list = []
+    min_seats_list = []
+    min_percents_list = []
+    
+    for part in recom_chain:
+        cut_edges_list.append(len(part["cut_edges"]))
+        min_percents_list.append(part["maj-min"].percents("min"))
+        min_seats = (np.array(part["maj-min"].percents("min")) >= min_win_thresh).sum()
+        min_seats_list.append(min_seats)
+    
+    return [cut_edges_list,min_seats_list,min_percents_list]
+    
+    
+    
